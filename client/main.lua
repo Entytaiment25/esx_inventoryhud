13f371347e23ac95b5e2de38a6b9a95d56d71ae4
enty <64799287+Entytaiment25@users.noreply.github.com>
2023-02-22 20:15:33 +0100

    ESX Legacy compatible

--- client/main.lua
+++ client/main.lua
@@ -77,21 +77,7 @@ local Keys = {
 }
 
 isInInventory = false
-ESX = nil
-
-Citizen.CreateThread(
-    function()
-        while ESX == nil do
-            TriggerEvent(
-                "esx:getSharedObject",
-                function(obj)
-                    ESX = obj
-                end
-            )
-            Citizen.Wait(0)
-        end
-    end
-)
+ESX = exports["es_extended"]:getSharedObject()
 
 Citizen.CreateThread(
     function()
